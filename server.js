1d8ff5281e562bf198f20e4d2209aec4ab20b326
mdArman2507 <mdarman2507@gmail.com>
2023-10-16 16:53:10 +0530

    Add body-parser

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // 1. Import express
 import express from 'express';
 import productRouter from './src/features/product/product.routes.js';
+import bodyParser from 'body-parser';
 
 // 2. Create Server
 const server=express();
+
+server.use(bodyParser.json());
 
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/products
--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -6,6 +6,9 @@ export default class ProductController{
         res.status(200).send(products);
     }
     addProduct(req,res){
+        console.log(req.body);
+        console.log("this is a post request");
+        res.status(200).send("post request recieved");
 
     }
     rateProduct(req,res){
