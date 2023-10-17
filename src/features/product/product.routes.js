58f45866e167509086f42cc5da135e0eae16bcd1
mdArman2507 <mdarman2507@gmail.com>
2023-10-17 10:55:07 +0530

    getOneProduct

--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -4,14 +4,26 @@
 import express from 'express';
 import ProductController from './product.controller.js';
 import {upload} from '../../middlewares/fileupload.middleware.js';
+
 // 2. Initialize Express router.
 const productRouter = express.Router();
 const productController = new ProductController();
 
 // All the paths to the controller methods.
 // localhost/api/products 
-productRouter.get('/', productController.getAllProducts);
-productRouter.post('/', upload.single('imageUrl'),productController.addProduct);
+productRouter.get(
+    '/', 
+productController.getAllProducts
+);
+productRouter.post(
+    '/', 
+upload.single('imageUrl'), 
+productController.addProduct
+);
+productRouter.get(
+    '/:id',
+    productController.getOneProduct
+);
 
 
 export default productRouter;
