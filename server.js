9fff7aa9412c9b17d517a0558e9d1c0c8839631c
mdArman2507 <mdarman2507@gmail.com>
2023-10-22 18:37:37 +0530

    add swageer 2.0

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import userRouter from './src/features/user/user.routes.js';
 import bodyParser from 'body-parser';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import cartRouter from './src/features/cartItems/cartItems.routes.js';
-
+import swagger from 'swagger-ui-express';
+import apiDocs from './swagger.json' assert {type:'json'};
 // 2. Create Server
 const server=express();
 
@@ -13,9 +14,11 @@ server.use(bodyParser.json());
 
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/products
+server.use("/api/docs",swagger.serve,swagger.setup(apiDocs));
 server.use("/api/products",jwtAuth, productRouter);
 server.use("/api/cartItems",jwtAuth,cartRouter);
 server.use("/api/users",userRouter);
+
 
 // 3. Default request handler
 server.get('/',(req,res)=>{
