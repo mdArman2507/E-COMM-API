77f89e9a236dc3f9de1bcb53f9d65133c04f870e
mdArman2507 <mdarman2507@gmail.com>
2023-10-24 14:19:47 +0530

    Add logger-3

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -10,8 +10,11 @@ async function log(logData){
 }
 const loggerMiddleWare=async(req,res,next)=>{
     // 1. Log request body
+    if(!req.url.includes('signin'))
+    {
     const logData=`${req.url}-${JSON.stringify(req.body)}`;
     await log(logData);
+    }
     next();
 }
 export default loggerMiddleWare;
